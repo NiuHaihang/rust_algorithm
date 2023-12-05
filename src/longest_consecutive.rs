f40e6032b8f7ec56733e266502fa2061d8afa1aa
niuhaihang <haihangniu@gmail.com>
2023-12-06 00:01:09 +0800

    MOD:longest

--- src/longest_consecutive.rs
+++ src/longest_consecutive.rs
@@ -1,48 +1,71 @@
-
-pub mod longest{
+pub mod longest {
+    use std::cmp::max;
+    use std::collections::HashMap;
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut mp: HashMap<i32, i32> = HashMap::new();
 
+        let mut uf = build_uf(n.try_into().unwrap());
+
+        for (i, num) in nums.iter().enumerate() {
+            match mp.get(num) {
+                Some(&i32) => continue,
+                None => {}
+            }
+            match mp.get(&(num - 1)) {
+                Some(&i32) => uf.union(i as i32, uf.find(mp.get(&(num - 1)).copied().unwrap())),
+                None => {}
+            }
+            match mp.get(&(num + 1)) {
+                Some(&i32) => uf.union(i as i32, uf.find(mp.get(&(num + 1)).copied().unwrap())),
+                None => {}
+            }
+            mp.insert(*num, i as i32);
+        }
+
+        let mut res = 0;
+        for (i, size) in uf.size.iter().enumerate() {
+            res = max(res, *size)
+        }
+        return res;
     }
 
-    struct UF{
+    struct UF {
         pre: Vec<i32>,
         size: Vec<i32>,
     }
+    fn build_uf(n: i32) -> UF {
+        let mut pre: Vec<i32> = Vec::new();
+        let mut size: Vec<i32> = vec![1, n];
 
-    impl UF{
-        fn new(n:i32)->UF{
-            let mut pre:Vec<i32> = Vec::new();
-            let mut size:Vec<i32>=vec![1,n];
-
-            for i in 1..n{
-                pre.push(i);
-            }
-            UF{
-                pre:pre,
-                size:size,
-            }
+        for i in 1..n {
+            pre.push(i);
         }
-
-        fn find(&self, i:i32)->i32{
-            if self.pre[i] == i{
+        UF {
+            pre: pre,
+            size: size,
+        }
+    }
+    impl UF {
+        fn find(&mut self, i: i32) -> i32 {
+            if self.pre[i as usize] == i {
                 return i;
             }
-            self.pre[i] = find(self.pre[i]);
-            self.pre[i]
+            self.pre[i as usize] = self.find(self.pre[i as usize]);
+            self.pre[i as usize]
         }
 
-        fn union(&self,i:i32,j:i32){
+        fn union(&mut self, i: i32, j: i32) {
             let x = self.find(i);
             let y = self.find(j);
 
-            if x < y{
-                self.pre[x] = y;
-                self.size[y] += self.size[x];
-                return
+            if x < y {
+                self.pre[x as usize] = y;
+                self.size[y as usize] += self.size[x as usize];
+                return;
             }
-            self.pre[y] = x;
-            self.size[x] += self.size[y];
+            self.pre[y as usize] = x;
+            self.size[x as usize] += self.size[y as usize];
         }
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod good_indices;
 mod is_palindrome;
+mod longest_consecutive;
 mod two_sum;
 
 use crate::good_indices::good::good_indices;
 use crate::is_palindrome::palindrome::is_palindrome;
+use crate::longest_consecutive::longest::longest_consecutive;
 use crate::two_sum::sum::two_sum;
 
 fn main() {
-    let nums = vec![2, 1, 1, 1, 3, 4, 1];
-    let k = 2;
-    let res = good_indices(nums, k);
+    let nums = vec![100, 4, 200, 1, 3, 2];
+    let res = longest_consecutive(nums);
     println!("res is:{:?}", res);
 }
