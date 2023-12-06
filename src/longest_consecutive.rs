83ab00f34a2fe4a0e148a805f03e50be4d4b2d06
niuhaihang <haihangniu@gmail.com>
2023-12-06 22:16:29 +0800

    MOD:longest_consecutive1

--- src/longest_consecutive.rs
+++ src/longest_consecutive.rs
@@ -1,7 +1,8 @@
 pub mod longest {
+    use core::num;
     use std::cmp::max;
-    use std::collections::HashMap;
-    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+    use std::collections::{HashMap, HashSet};
+    pub fn longest_consecutive1(nums: Vec<i32>) -> i32 {
         let n = nums.len();
         let mut mp: HashMap<i32, i32> = HashMap::new();
 
@@ -13,11 +14,17 @@ pub mod longest {
                 None => {}
             }
             match mp.get(&(num - 1)) {
-                Some(&i32) => uf.union(i as i32, uf.find(mp.get(&(num - 1)).copied().unwrap())),
+                Some(&i32) => {
+                    let mut j = uf.find(mp.get(&(num - 1)).copied().unwrap());
+                    uf.union(i as i32, j);
+                }
                 None => {}
             }
             match mp.get(&(num + 1)) {
-                Some(&i32) => uf.union(i as i32, uf.find(mp.get(&(num + 1)).copied().unwrap())),
+                Some(&i32) => {
+                    let mut j = uf.find(mp.get(&(num + 1)).copied().unwrap());
+                    uf.union(i as i32, j);
+                }
                 None => {}
             }
             mp.insert(*num, i as i32);
@@ -30,15 +37,33 @@ pub mod longest {
         return res;
     }
 
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        let s = nums.into_iter().collect::<HashSet<i32>>();
+        let mut tmp_len;
+        let mut res = 0;
+        for &val in s.iter(){
+            if !s.contains(&(val-1)){
+                tmp_len = 1;
+                let mut tmp_val = val+1;
+                while s.contains(&tmp_val){
+                    tmp_len += 1;
+                    tmp_val+=1;
+                }
+                res = res.max(tmp_len);
+            }
+        }
+        res
+    }
+
     struct UF {
         pre: Vec<i32>,
         size: Vec<i32>,
     }
     fn build_uf(n: i32) -> UF {
         let mut pre: Vec<i32> = Vec::new();
-        let mut size: Vec<i32> = vec![1, n];
+        let mut size: Vec<i32> = vec![1; n as usize];
 
-        for i in 1..n {
+        for i in 0..n {
             pre.push(i);
         }
         UF {
